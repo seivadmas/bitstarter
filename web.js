ba741de6da2e24cf3a478135d91c761e0e8bdb0a
Sam Philip <seivadmas@gmail.com>
2013-07-08 17:44:49 +0000

    Test string

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(indexString);
+  response.send("TestString");
 });
 
 var port = process.env.PORT || 5000;
